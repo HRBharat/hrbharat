8d0ae43888bbba66bb81df1b0a4f7a38aa5a9217
HRBharat <gokulgopakumar5556@gmail.com>
2026-05-29 18:30:10 +0400

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
+const withPWA = require('@ducanh2912/next-pwa').default({
+  dest: 'public',
+  cacheOnFrontEndNav: true,
+  aggressiveFrontEndNavCaching: true,
+  reloadOnOnline: true,
+  swMinify: true,
+  disable: process.env.NODE_ENV === 'development',
+});
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  /* Additional environment options can be appended cleanly here */
+  // Your existing Next.js config options here (like images, redirects, etc.)
 };
 
-module.exports = nextConfig;
+module.exports = withPWA(nextConfig);
